22ef45aa13fc7b5ea35c6024d1ffdd7b62a45127
bOk <kohbokyan@gmail.com>
2018-10-31 12:00:12 +0800

    new LoginPage() created

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'routing';
+import 'auth/login.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      home:new LoginPage(),
         initialRoute: '/login',
         routes: routes
     );
