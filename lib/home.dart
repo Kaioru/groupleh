723fbe1d5ff75a01fee37ce33a70f587173386e0
bOk <kohbokyan@gmail.com>
2018-11-07 11:41:25 +0800

    delete chat_screen.dart

---
+++ lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+
+class GroupLehHome extends StatefulWidget {
+  @override
+  _GroupLehHomeState createState() => _GroupLehHomeState();
+}
+
+class _GroupLehHomeState extends State<GroupLehHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("GroupLeh"),
+      ),
+
+      body: new Column(
+
+      )
+    );
+  }
+}
