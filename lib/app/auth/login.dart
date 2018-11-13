d7335df75ae9231bb776ecfcf8bda75a10f0547c
Kaioru <keifuurawrs@gmail.com>
2018-11-13 17:05:03 +0800

    Add basic login checks and email validations

--- lib/app/auth/login.dart
+++ lib/app/auth/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:groupleh/app/app.dart';
 import 'package:groupleh/app/auth/register.dart';
 
 class Login extends StatefulWidget {
@@ -8,6 +11,14 @@ class Login extends StatefulWidget {
 
 class _Login extends State<Login> {
   final _formKey = GlobalKey<_Login>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future<FirebaseUser> _handleSignIn() async {
+    return await _auth.signInWithEmailAndPassword(
+        email: _emailController.text, password: _passwordController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +33,22 @@ class _Login extends State<Login> {
                   children: <Widget>[
                     FlutterLogo(size: 120.0),
                     TextFormField(
+                        controller: _emailController,
+                        validator: (val) {
+                          Pattern pattern =
+                              r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                          RegExp regex = new RegExp(pattern);
+                          if (!regex.hasMatch(val))
+                            return 'Please enter a valid email!';
+                          else
+                            return null;
+                        },
                         decoration: new InputDecoration(
                           labelText: "Email",
                         ),
                         keyboardType: TextInputType.emailAddress),
                     TextFormField(
+                        controller: _passwordController,
                         decoration: new InputDecoration(
                           labelText: "Password",
                         ),
@@ -43,7 +65,9 @@ class _Login extends State<Login> {
                         child: Text('Register'),
                       ),
                       RaisedButton(
-                        onPressed: () {},
+                        onPressed: () async => await _handleSignIn()
+                            .then((user) => print(user))
+                            .catchError((e) => print(e)),
                         child: Text('Login'),
                       )
                     ])
