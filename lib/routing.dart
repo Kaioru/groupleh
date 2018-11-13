579d233c56148f51c7a0d0fc2838e0aea9f0b725
Kaioru <keifuurawrs@gmail.com>
2018-11-13 12:06:12 +0800

    Cleanup routing

--- lib/routing.dart
+++ lib/routing.dart
@@ -1,10 +1,9 @@
-import 'home.dart';
-import 'auth/login.dart';
-import 'auth/registration.dart';
+import 'package:groupleh/app/app.dart';
+import 'package:groupleh/auth/login.dart';
+import 'package:groupleh/auth/registration.dart';
 
 final routes = {
-  '/': (context) => new HomePage(),
+  '/': (context) => new App(),
   '/login': (context) => new LoginPage(),
   '/register': (context) => new RegistrationPage(),
-
 };
