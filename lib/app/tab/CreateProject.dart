8d2014a887efbb8076240777a7a0f8610ed86abe
Meng Ke Marcus Goh <s10182762@connect.np.edu.sg>
2018-11-14 15:18:30 +0800

    Update lib/app/tab/CreateProject.dart

--- lib/app/tab/CreateProject.dart
+++ lib/app/tab/CreateProject.dart
@@ -7,3 +7,22 @@ enum LeaveBehindDemoAction {
   rightSwipe
 }
 
+void main() {
+  runApp(new MyApp());
+}
+
+class LeaveBehindItem implements Comparable<LeaveBehindItem> {
+  LeaveBehindItem({ this.index, this.name, this.subject, this.body });
+
+  LeaveBehindItem.from(LeaveBehindItem item)
+      : index = item.index, name = item.name, subject = item.subject, body = item.body;
+
+  final int index;
+  final String name;
+  final String subject;
+  final String body;
+
+  @override
+  int compareTo(LeaveBehindItem other) => index.compareTo(other.index);
+}
+
