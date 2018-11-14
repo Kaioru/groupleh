94ab90555ef4b5e75c60292b0e79cda742886400
Meng Ke Marcus Goh <s10182762@connect.np.edu.sg>
2018-11-14 15:18:03 +0800

    Update lib/app/tab/CreateProject.dart

--- lib/app/tab/CreateProject.dart
+++ lib/app/tab/CreateProject.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:collection/collection.dart' show lowerBound;
+enum LeaveBehindDemoAction {
+  reset,
+  horizontalSwipe,
+  leftSwipe,
+  rightSwipe
+}
+
