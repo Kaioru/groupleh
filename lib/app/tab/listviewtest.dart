aeb9f7058e43cf00f6830a1d9665c3f2f9906247
bOk <kohbokyan@gmail.com>
2019-01-07 16:08:43 +0800

    Add ListWheelScrollView

--- lib/app/tab/listviewtest.dart
+++ lib/app/tab/listviewtest.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:groupleh/app/app_state.dart';
 
-const List<String> learningStyle = ["Collaborative", "Focused"];
 
 class LoopWheel extends StatefulWidget {
   final AppState state;
@@ -11,58 +10,70 @@ class LoopWheel extends StatefulWidget {
   State<StatefulWidget> createState() => _LoopWheel(state);
 }
 
-
 class _LoopWheel extends State<LoopWheel> {
+  FixedExtentScrollController fixedExtentScrollController;
   final AppState state;
   _LoopWheel(this.state);
-
   @override
   Widget build(BuildContext context) {
     final _style = Theme.of(context).textTheme.display2;
     return new Scaffold(
-      body: Container(
-        child:new Center(
-          child: new ConstrainedBox(
-            constraints: BoxConstraints(
-              // Set height to one line, otherwise the whole vertical space is occupied.
-              maxHeight: _style.fontSize,
-            ),
-            child: new ListWheelScrollView.useDelegate(
-              itemExtent: _style.fontSize,
-              childDelegate: ListWheelChildLoopingListDelegate(
-                children: List<Widget>.generate(
-                10, (index) => Text('${index + 1}', style: _style),
+      body: Center(
+        child:Container(
+          height: _style.fontSize,
+            child:Row(
+              children: <Widget>[
+                Expanded(
+                  flex:4,
+                  child: Container(
+                    child: new ListWheelScrollView.useDelegate(
+                  
+                      physics: FixedExtentScrollPhysics(),
+                      itemExtent: _style.fontSize,
+                      childDelegate: ListWheelChildLoopingListDelegate(
+                        children: const <Widget>[
+                          Text("Collaborative", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text("Focused", style: TextStyle(fontWeight: FontWeight.bold)),
+                        ]
+                      ),
+                    ),
+                  )
                 ),
-              ),
+                Expanded(
+                  flex:2,
+                  child: Container(
+                    child: new ListWheelScrollView.useDelegate(
+                  
+                      physics: FixedExtentScrollPhysics(),
+                      itemExtent: _style.fontSize,
+                      childDelegate: ListWheelChildLoopingListDelegate(
+                        children: const <Widget>[
+                          Text("3-4", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text("5-6", style: TextStyle(fontWeight: FontWeight.bold)),
+                        ]
+                      ),
+                    ),
+                  )
+                ),
+                Expanded(
+                  flex:4,
+                  child: Container(
+                    child: new ListWheelScrollView.useDelegate(
+                      physics: FixedExtentScrollPhysics(),
+                      itemExtent: _style.fontSize,
+                      childDelegate: ListWheelChildLoopingListDelegate(
+                        children: const <Widget>[
+                          Text("Morning", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text("Night", style: TextStyle(fontWeight: FontWeight.bold)),
+                        ]
+                      ),
+                    ),
+                  )
+                ),
+              ],
             ),
           ),
         ),
-      )
-    );
-
-  }
-
-//  Widget ListBuildLS(){
-//    final _style = Theme
-//        .of(context)
-//        .textTheme
-//        .display2;
-//    new Center(
-//      child: new ConstrainedBox(
-//        constraints: BoxConstraints(
-//          // Set height to one line, otherwise the whole vertical space is occupied.
-//          maxHeight: _style.fontSize,
-//        ),
-//        child: new ListWheelScrollView.useDelegate(
-//          itemExtent: _style.fontSize,
-//          childDelegate: ListWheelChildLoopingListDelegate(
-//            children: <Widget>[
-//              Text("Collaborative", style:TextStyle(fontWeight: FontWeight.bold)),
-//              Text("Focused", style:TextStyle(fontWeight: FontWeight.bold)),
-//            ],
-//          ),
-//        ),
-//      ),
-//    );
-//  }
+      );
+    }
 }
