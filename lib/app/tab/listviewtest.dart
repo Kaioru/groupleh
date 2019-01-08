eb1ad504a171051a8da540c47c3918d9b725baa7
bOk <kohbokyan@gmail.com>
2019-01-08 09:25:29 +0800

    Created Widgets for ScrollWheel

--- lib/app/tab/listviewtest.dart
+++ lib/app/tab/listviewtest.dart
@@ -16,64 +16,76 @@ class _LoopWheel extends State<LoopWheel> {
   _LoopWheel(this.state);
   @override
   Widget build(BuildContext context) {
-    final _style = Theme.of(context).textTheme.display2;
+    final _style = Theme.of(context).textTheme.display2;  
     return new Scaffold(
       body: Center(
         child:Container(
           height: _style.fontSize,
             child:Row(
               children: <Widget>[
-                Expanded(
-                  flex:4,
-                  child: Container(
-                    child: new ListWheelScrollView.useDelegate(
-                  
-                      physics: FixedExtentScrollPhysics(),
-                      itemExtent: _style.fontSize,
-                      childDelegate: ListWheelChildLoopingListDelegate(
-                        children: const <Widget>[
-                          Text("Collaborative", style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text("Focused", style: TextStyle(fontWeight: FontWeight.bold)),
-                        ]
-                      ),
-                    ),
-                  )
-                ),
-                Expanded(
-                  flex:2,
-                  child: Container(
-                    child: new ListWheelScrollView.useDelegate(
-                  
-                      physics: FixedExtentScrollPhysics(),
-                      itemExtent: _style.fontSize,
-                      childDelegate: ListWheelChildLoopingListDelegate(
-                        children: const <Widget>[
-                          Text("3-4", style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text("5-6", style: TextStyle(fontWeight: FontWeight.bold)),
-                        ]
-                      ),
-                    ),
-                  )
-                ),
-                Expanded(
-                  flex:4,
-                  child: Container(
-                    child: new ListWheelScrollView.useDelegate(
-                      physics: FixedExtentScrollPhysics(),
-                      itemExtent: _style.fontSize,
-                      childDelegate: ListWheelChildLoopingListDelegate(
-                        children: const <Widget>[
-                          Text("Morning", style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text("Night", style: TextStyle(fontWeight: FontWeight.bold)),
-                        ]
-                      ),
-                    ),
-                  )
-                ),
-              ],
-            ),
+                _scrollLearningStyle(),
+                _scrollNumberOfPeople(),
+                _scrollTimeOfDay(),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
+
+  Widget _scrollLearningStyle(){
+    return Expanded(
+      flex:4,
+      child: Container(
+        child: new ListWheelScrollView.useDelegate(
+      
+          physics: FixedExtentScrollPhysics(),
+          itemExtent: _style.fontSize,
+          childDelegate: ListWheelChildLoopingListDelegate(
+            children: const <Widget>[
+              Text("Collaborative", style: TextStyle(fontWeight: FontWeight.bold)),
+              Text("Focused", style: TextStyle(fontWeight: FontWeight.bold)),
+            ]
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget _scrollNumberOfPeople(){
+    return Expanded(
+      flex:2,
+      child: Container(
+        child: new ListWheelScrollView.useDelegate(
+      
+          physics: FixedExtentScrollPhysics(),
+          itemExtent: _style.fontSize,
+          childDelegate: ListWheelChildLoopingListDelegate(
+            children: const <Widget>[
+              Text("3-4", style: TextStyle(fontWeight: FontWeight.bold)),
+              Text("5-6", style: TextStyle(fontWeight: FontWeight.bold)),
+            ]
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget _scrollTimeOfDay(){
+    return Expanded(        
+      flex:4,
+      child: Container(
+        child: new ListWheelScrollView.useDelegate(
+          physics: FixedExtentScrollPhysics(),
+          itemExtent: _style.fontSize,
+          childDelegate: ListWheelChildLoopingListDelegate(
+            children: const <Widget>[
+              Text("Morning", style: TextStyle(fontWeight: FontWeight.bold)),
+              Text("Night", style: TextStyle(fontWeight: FontWeight.bold)),
+            ]
+          ),
+        ),
+      )
+    );
+  }
 }
