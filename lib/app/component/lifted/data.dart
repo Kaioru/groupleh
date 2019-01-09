3e60faf8e6f1c30e5cd7146b6e10f1d0e1557ce1
Kaioru <keifuurawrs@gmail.com>
2019-01-09 09:20:11 +0800

    Add placeholder card swiping

---
+++ lib/app/component/lifted/data.dart
@@ -0,0 +1,3 @@
+import 'package:groupleh/app/component/lifted/styles.dart';
+
+List imageData = [image5, image3, image4, image2, image1];
--- lib/app/project/project.dart
+++ lib/app/project/project.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:groupleh/app/component/lifted/index.dart';
 
 class Project extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: CardDemo(),
+    );
   }
 }
