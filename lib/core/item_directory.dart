150d4343a696efde1def7310f217017962c24317
Swazztastic <35524308+Swazztastic@users.noreply.github.com>
2019-01-13 18:45:53 +0800

    Update item_directory.dart

--- lib/core/item_directory.dart
+++ lib/core/item_directory.dart
@@ -1,10 +1,5 @@
-import 'dart:typed_data';
-
-import 'package:flutter/services.dart';
 import 'package:groupleh/core/item.dart';
 import 'package:groupleh/core/item_type.dart';
-import 'package:groupleh/core/profile.dart';
-import 'package:image/image.dart';
 
 class ItemDirectory {
   Map<String, Item> items;
@@ -23,48 +18,11 @@ class ItemDirectory {
       Item(type: ItemType.hair, prefab: "hair_two_block_green"),
       Item(type: ItemType.top, prefab: "top_cowboy_blue"),
       Item(type: ItemType.top, prefab: "top_layered_shirt_black"),
-      Item(type: ItemType.top, prefab: "top_leafa_green"),
-      Item(type: ItemType.top, prefab: "top_shirt_white"),
-      Item(type: ItemType.bottom, prefab: "bottom_leafa_green"),
-      Item(
-          type: ItemType.bottom,
-          prefab: "bottom_short_blue",
-          name: "Blue Shorts",
-          desc: "Blue is for cool people."),
-      Item(
-          type: ItemType.bottom,
-          prefab: "bottom_short_brown",
-          name: "Brown Shorts",
-          desc: "Brown is the colour of poo."),
+      Item(type: ItemType.top, prefab: "top_leafa_green"), name: "Green Shirt", desc: "Just a few more pieces of apparel till you can stand at the edge of a rainbow."),
+      Item(type: ItemType.top, prefab: "top_shirt_white"), name: "White Shirt", desc: "Plain and Simple, no need for fancy shirts when you have fancy hair."),
+      Item(type: ItemType.bottom, prefab: "bottom_leafa_green"), name: "Green Shorts", desc: "For all you environmentalist folks out there..."),
+      Item(type: ItemType.bottom, prefab: "bottom_short_blue"), name: "Blue Shorts", desc: "Also known as jeans."),
+      Item(type: ItemType.bottom, prefab: "bottom_short_brown", name: "Brown Shorts", desc: "Brown is the colour of poo."),
     ]).forEach((i) => items[i.prefab] = i);
-  }
-
-  Future<Uint8List> image(String key) async {
-    Uint8List data = (await rootBundle.load(key)).buffer.asUint8List();
-    return data;
-  }
-
-  Future<List<int>> generateHairPreview(String wardrobeHair) async {
-    var prefix = "assets/images/wardrobe/";
-    Image avatar = decodePng(await image(prefix + "avatar_head_only.png"));
-    Image hair = decodePng(await image(prefix + wardrobeHair + ".png"));
-    drawImage(avatar, hair);
-
-    return encodePng(avatar);
-  }
-
-  Future<List<int>> generate(
-      String wardrobeHair, String wardrobeTop, String wardrobeBottom) async {
-    var prefix = "assets/images/wardrobe/";
-    Image avatar = decodePng(await image(prefix + "avatar_white.png"));
-    Image hair = decodePng(await image(prefix + wardrobeHair + ".png"));
-    Image top = decodePng(await image(prefix + wardrobeTop + ".png"));
-    Image bottom = decodePng(await image(prefix + wardrobeBottom + ".png"));
-
-    drawImage(avatar, hair);
-    drawImage(avatar, top);
-    drawImage(avatar, bottom);
-
-    return encodePng(avatar);
   }
 }
