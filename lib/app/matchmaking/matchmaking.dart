f0ee4f9320cf14fe493267f527fc5454494870da
bOk <kohbokyan@gmail.com>
2019-01-30 01:54:03 +0800

    daberooni i am keith

--- lib/app/matchmaking/matchmaking.dart
+++ lib/app/matchmaking/matchmaking.dart
@@ -11,12 +11,27 @@ class Matchmaking extends StatefulWidget {
 
 class _Matchmaking extends State<Matchmaking> {
   FixedExtentScrollController fixedExtentScrollController;
+  PageController _pageController = PageController(initialPage: 0, viewportFraction: 1.0);
   final AppState state;
   _Matchmaking(this.state);
   @override
   Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          learningStylePage(),
+          numberPage(),
+          timeOfDayPage(),
+        ],
+      )
+    );
+  }
+
+  Widget learningStylePage(){
     final _style = Theme.of(context).textTheme.display2;  
-    return new Scaffold(
+    return Scaffold(
       body: Center(
         child: Container(
           child: Column(
@@ -42,39 +57,28 @@ class _Matchmaking extends State<Matchmaking> {
                   ],
                 ),
               ),
-              Container(
-                padding:EdgeInsets.only(top:30.0, bottom:20.0),
-                child: Text("Step 1: Learning style?", style: TextStyle(fontSize: 30)),
-              ),
-          
-              Container(
-              height: _style.fontSize,
-                child: _scrollLearningStyle(),
-              ),
+              Expanded(
+                child: Column( 
+                  mainAxisAlignment: MainAxisAlignment.end, 
+                  children: <Widget>[
+                    Container(
+                      padding:EdgeInsets.only(top:30.0, bottom:20.0),
+                      child: Text("Step 1: Learning style?", style: TextStyle(fontSize: 30)),
+                    ),
+                
+                    Container(
+                    height: _style.fontSize,
+                      child: _scrollLearningStyle(),
+                    ),
 
-              Container(
-                padding:EdgeInsets.only(top:30.0, bottom:20.0),
-                child: Text("Step 2: How many?", style: TextStyle(fontSize: 30)),
+                  ],
+                ),
               ),
-          
-              Container(
-              height: _style.fontSize,
-                child: _scrollNumberOfPeople(),
-              ),
-
-              Container(
-                padding:EdgeInsets.only(top:30.0, bottom:20.0),
-                child: Text("Step 3: When?", style: TextStyle(fontSize: 30)),
-              ),
-          
-              Container(
-              height: _style.fontSize,
-                child: _scrollTimeOfDay(),
-              ),
+              
               Expanded(
                 child: Align(
                   alignment: FractionalOffset(1, 1),
-                  child: buildPlatformSpecific()
+                  child: buildPlatformSpecificHome()
                 ),
               ),
             ],
@@ -84,7 +88,165 @@ class _Matchmaking extends State<Matchmaking> {
     );
   }
 
-  Widget buildPlatformSpecific(){
+  Widget numberPage(){
+    final _style = Theme.of(context).textTheme.display2;  
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top:40.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Almost there!!", 
+                    style: const TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 45.0)
+                    ),
+
+                    Text("We don't allow groups of 2 cus yall be dating",
+                    style: const TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontFamily: 'Poppins',
+                      fontSize:12.0)
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column( 
+                  mainAxisAlignment: MainAxisAlignment.end, 
+                  children: <Widget>[
+                    Container(
+                      padding:EdgeInsets.only(top:30.0, bottom:20.0),
+                      child: Text("Step 2: How many?", style: TextStyle(fontSize: 30)),
+                    ),
+                
+                    Container(
+                    height: _style.fontSize,
+                      child: _scrollNumberOfPeople(),
+                    ),
+
+                  ],
+                ),
+              ),           
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset(1, 1),
+                  child: ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox( 
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: RaisedButton(
+                          child: Icon(Icons.arrow_back, color: Color(0xFF303030)),
+                          onPressed: (){ gotoStyle();},
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width:MediaQuery.of(context).size.width / 3,
+                        child: RaisedButton(
+                          child: Icon(Icons.arrow_forward, color: Color(0xFF303030)),
+                          onPressed: (){gotoTime();},
+                          color: Color(0xFF00C6FF)
+                        ),
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget timeOfDayPage(){
+    final _style = Theme.of(context).textTheme.display2;  
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top:40.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Finish!", 
+                    style: const TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 45.0)
+                    ),
+
+                    Text("Good timing = great times",
+                    style: const TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontFamily: 'Poppins',
+                      fontSize:12.0)
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column( 
+                  mainAxisAlignment: MainAxisAlignment.end, 
+                  children: <Widget>[
+                    Container(
+                      padding:EdgeInsets.only(top:30.0, bottom:20.0),
+                      child: Text("Step 3: When?", style: TextStyle(fontSize: 30)),
+                    ),
+                
+                    Container(
+                    height: _style.fontSize,
+                      child: _scrollTimeOfDay(),
+                    ),
+
+                  ],
+                ),
+              ),
+              
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset(1, 1),
+                  child: ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox( 
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: RaisedButton(
+                          child: Icon(Icons.arrow_back, color: Color(0xFF303030)),
+                          onPressed: (){ gotoGroups();},
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width:MediaQuery.of(context).size.width / 3,
+                        child: RaisedButton(
+                          child: Icon(Icons.check, color: Color(0xFF303030)),
+                          // insert firebase configs here
+                          onPressed: (){},
+                          color: Color(0xFF00C6FF)
+                        ),
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildPlatformSpecificHome(){
     if(Theme.of(context).platform == TargetPlatform.iOS){
       return ButtonBar(
         alignment: MainAxisAlignment.center,
@@ -100,8 +262,8 @@ class _Matchmaking extends State<Matchmaking> {
           SizedBox(
             width:MediaQuery.of(context).size.width / 3,
             child: RaisedButton(
-              child: Icon(Icons.check_circle, color: Color(0xFF303030)),
-              onPressed: (){},
+              child: Icon(Icons.arrow_forward, color: Color(0xFF303030)),
+              onPressed: (){gotoGroups();},
               color: Color(0xFF00C6FF)
             ),
           ),
@@ -115,8 +277,8 @@ class _Matchmaking extends State<Matchmaking> {
           SizedBox(
             width:2 * (MediaQuery.of(context).size.width / 3),
             child: RaisedButton(
-              child: Icon(Icons.check_circle, color: Color(0xFF303030)),
-              onPressed: (){},
+              child: Icon(Icons.arrow_forward, color: Color(0xFF303030)),
+              onPressed: (){gotoGroups();},
               color: Color(0xFF00C6FF)
             ),
           ),
@@ -209,6 +371,31 @@ class _Matchmaking extends State<Matchmaking> {
           ),
         ],
       ),
+    );
+  }
+
+  gotoStyle() {
+    //controller_0To1.forward(from: 0.0);
+    _pageController.animateToPage(
+      0,
+      duration: Duration(milliseconds: 200),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+  gotoGroups() {
+    //controller_0To1.forward(from: 0.0);
+    _pageController.animateToPage(
+      1,
+      duration: Duration(milliseconds: 200),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+  gotoTime() {
+    //controller_0To1.forward(from: 0.0);
+    _pageController.animateToPage(
+      2,
+      duration: Duration(milliseconds: 200),
+      curve: Curves.fastOutSlowIn,
     );
   }
 }
